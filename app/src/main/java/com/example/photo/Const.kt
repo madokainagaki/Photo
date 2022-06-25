80516db9e4ec17cd678086f222f3b8135453f70e
madokainagaki <jo6gencho9@gmail.com>
2022-06-25 10:25:35 +0900

    firebase練習段階

---
+++ app/src/main/java/com/example/photo/Const.kt
@@ -0,0 +1,13 @@
+package com.example.photo
+
+// Firebaseにユーザの表示名を保存するパス
+const val UsersPATH = "users"
+
+// Firebaseに質問を保存するバス
+const val DiaryPATH = "diarys"
+
+// Firebaseに回答を保存するパス
+const val AnswersPATH = "answers"
+
+// Preferenceに表示名を保存する時のキー
+const val NameKEY = "name"
--- app/src/main/java/com/example/photo/MainActivity.kt
+++ app/src/main/java/com/example/photo/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.photo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Transformations.map
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.collections.Map as Map
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mDiaryAdapter: DiaryListAdapter
     private lateinit var mDiaryArrayList: ArrayList<Diary>
+    private lateinit var mDiaryRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +21,9 @@ class MainActivity : AppCompatActivity() {
         mDiaryAdapter = DiaryListAdapter(this)
         mDiaryArrayList = ArrayList<Diary>()
 
-        val title = "タイトル"
-        val content = "内容"
-        val name = "名前"
+        val title = "熱海旅行"
+        val content = "夏のグループ旅行"
+        val name = "iagaki"
         val diary = Diary(title, content, name)
 
         mDiaryArrayList.add(diary)
@@ -26,5 +31,27 @@ class MainActivity : AppCompatActivity() {
         mDiaryAdapter.mDiaryList = mDiaryArrayList
         diaryListView.adapter = mDiaryAdapter
         mDiaryAdapter.notifyDataSetChanged()
+
+        val dataBaseReference = FirebaseDatabase.getInstance().reference
+        val data2 = "●●の旅"
+
+        val testRef = dataBaseReference.child(DiaryPATH).child(data2)
+        val data = HashMap<String, String>()
+        data["title"] = "タイトルだよーん"
+        data["content"] = "内容だよーん"
+        data["name"] = "名前だよーん"
+        testRef.push().setValue(data)
+
+        val data3 = HashMap<String, String>()
+        val testRef3 = dataBaseReference.child(DiaryPATH).child(data2).child("-N5NLnkjj-5_dFEFL13v")
+        data3["title"] = "変更だよーん"
+        data3["content"] = "変更だよーん"
+        data3["name"] = "変更だよーん"
+        testRef3.setValue(data3)
+
+        toLoginPage.setOnClickListener{
+//        val intent = Intent(applicationContext, LoginActivity::class.java)
+//        startActivity(intent)
+        }
     }
 }
